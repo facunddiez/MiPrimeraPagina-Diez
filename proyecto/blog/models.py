876516e71a3d885bb51e584cb8e53fa8ef7e4169
Facundo Diez <facundobdiez@gmail.com>
2024-01-12 11:04:39 -0300

    add Modelos y admin

---
+++ proyecto/blog/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    contenido = models.TextField()
+    fecha_publicacion = models.DateTimeField('Fecha de publicación')
+    autor = models.ForeignKey('Autor', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+    
+class Comentario(models.Model):
+    post = models.ForeignKey('Post', null=True, blank =True, on_delete=models.SET_NULL)
+    autor = models.CharField(max_length=100)
+    contenido = models.TextField()
+    fecha_comentario = models.DateTimeField('Fecha de comentario')
+
+    def __str__(self):
+        return self.contenido
+    
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    bio = models.TextField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nombre
